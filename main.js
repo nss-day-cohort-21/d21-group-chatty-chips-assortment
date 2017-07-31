13c81563144719faf4360b411d385533dbef94db
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-07-31 15:59:18 -0500

    updated and working on check marks

--- main.js
+++ main.js
@@ -22,9 +22,13 @@
 let input = document.getElementById("textInput");
 let createButton = document.getElementById("createButton");
 let outputCard = document.getElementById("greetingCardMain");
+let darkText = document.getElementById("darkTheme");
+let largeText = document.getElementById("largeText");
+let darkArea = document.getElementById("darkStuff");
 // let outputText = document.getElementById("userMessage");
 createButton.addEventListener("click", function(){
     createCard();
+
     // userMessage();
 let outputText = document.getElementById("greetingCardMain");
 outputText.innerHTML += input.value;
@@ -43,3 +47,12 @@ function createCard() {
          `;
 outputCard.innerHTML += greetingCard;
 }
+
+.addEventListener("click", function(){
+if (darkText.checked === true) {
+		darkArea.classList.toggle("dark");	
+} else if (largeText.checked === true) {
+		darkArea.classList.toggle("big");
+}
+});
+
