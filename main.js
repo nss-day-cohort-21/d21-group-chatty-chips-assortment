aec447aa0f157dfb456395b36cf06ff4071278fc
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-08-01 13:56:58 -0500

    check boxes working

--- main.js
+++ main.js
@@ -4,20 +4,11 @@ console.log("this is the main.js speaking!")
 
 let input = document.getElementById("textInput");
 let createButton = document.getElementById("createButton");
-let outputCard = document.getElementById("greetingCardMain");
-let darkText = document.getElementById("darkTheme");
+let outputCard = document.getElementById("messageMain");
 let largeText = document.getElementById("largeText");
-let darkArea = document.getElementById("darkStuff");
-// let outputText = document.getElementById("userMessage");
-createButton.addEventListener("click", function(){
-    createCard();
-
-    // userMessage();
-let outputText = document.getElementById("greetingCardMain");
-outputText.innerHTML += input.value;
-// let deleteButton = document.getElementById("deleteButton");
-// console.log(outputText);
-// let garbage = deleteButton.removeChild(outputCard);
+let wholeArea = document.getElementById("wholeArea");
+let darkText = document.getElementById("darkTheme");
+let wholeBig = document.getElementById("wholeBigText");
 
 //Add an event listener to the SEND button: 
 createButton.addEventListener("click", function() {
@@ -30,7 +21,7 @@ createButton.addEventListener("click", function() {
     // same as the button click function();
 
     //Define the outputText action
-    let outputText = document.getElementById("greetingCardMain");
+    let outputText = document.getElementById("messageMain");
     outputText.innerHTML += input.value;
 });
 
@@ -39,22 +30,23 @@ createButton.addEventListener("click", function() {
 
 function createCard() {
     let greetingCard =
-        `<h3 id="userMessage"></h3>
-         </div><hr>
-        <button id="deleteButton">Delete</button> 
-         `;
-
-outputCard.innerHTML += greetingCard;
-}
-
-.addEventListener("click", function(){
-if (darkText.checked === true) {
-		darkArea.classList.toggle("dark");	
-} else if (largeText.checked === true) {
-		darkArea.classList.toggle("big");
-}
-});
-
+        `
+        <br>
+        
+         <hr>
+         <button id="deleteButton">Delete</button> 
+         <h3 id="userMessage"></h3>
+        `;
     outputCard.innerHTML += greetingCard;
+}
+
+//This makes the radio buttons change depending on which one is clicked
+darkText.addEventListener("change", toggleDark);
+function toggleDark() {
+        wholeBig.classList.toggle("dark");  
+}
+largeText.addEventListener("change", toggleLarge);
+function toggleLarge() {
+    wholeBig.classList.toggle("big");
 }
 
